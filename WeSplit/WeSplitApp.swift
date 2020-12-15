42add64b6cebb2f96e81d90326ea1f7c2d4656e0
austin hill <hilldog1378@gmail.com>
2020-12-14 19:39:40 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Austin Hill on 12/14/20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
